1f0b20b692d677a252d8fbd9113ef48415284a5e
Davletgareeva11801 <alsu.davletgareeva.2013@mail.ru>
2021-10-22 00:07:47 +0300

    working with dio

--- lib/cats_screen.dart
+++ lib/cats_screen.dart
@@ -1,5 +1,6 @@
 import 'dart:convert';
 
+import 'package:dio/dio.dart';
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 
@@ -9,31 +10,43 @@ import 'models/cat.dart';
 class Cats extends StatefulWidget {
   const Cats({Key? key, required this.title}) : super(key: key);
   final String title;
+
   @override
   _CatsState createState() => _CatsState();
 }
 
 class _CatsState extends State<Cats> {
-
   List<Cat>? _cats = [];
+  String url = "https://api.thecatapi.com/v1/images/search?breed_ids=beng";
 
   TextEditingController _textEditingController = TextEditingController();
 
   @override
-  void initState(){
+  void initState() {
     super.initState();
-    _getCats();
+    _getCatsWithDio();
   }
 
   Future<void> _getCats() async {
-    // var response = await http.get(Uri.parse("https://itis-chat-app-ex.herokuapp.com/chat"));
-    var response = await http.get(Uri.parse("https://api.thecatapi.com/v1/images/search?breed_ids=beng&include_breeds=true"));
+    var response = await http.get(Uri.parse(url));
     setState(() {
       _cats = catFromJson(response.body);
     });
+  }
 
+  Future<void> _getCatsWithDio() async {
+    // var response = await http.get(Uri.parse("https://itis-chat-app-ex.herokuapp.com/chat"));
+    Dio dio = Dio();
 
-    }
+    dio
+        .get(url)
+        .then((response) => setState(() {
+              _cats = catFromJson(jsonEncode(response.data));
+            }))
+        .catchError((error) {
+      print(error.toString());
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -44,51 +57,48 @@ class _CatsState extends State<Cats> {
         body: SafeArea(
           child: Center(
               child: Padding(
-                padding: const EdgeInsets.all(16.0),
-                child: Column(
-                  children: [
-                    Expanded(
-                      child: ListView(
-                        children: _cats?.map((item) {
+            padding: const EdgeInsets.all(16.0),
+            child: Column(
+              children: [
+                Expanded(
+                  child: ListView(
+                    children: _cats?.map((item) {
                           return ListTile(
-                            leading: Image.network(item.url),
+                              leading: Image.network(item.url),
                               title: Text(
                                 item.id,
                                 style: const TextStyle(
                                     color: Colors.deepPurple, fontSize: 16),
                               ),
                               onTap: () {
-
                                 Navigator.of(context).push(MaterialPageRoute(
-                                    builder: (context) => DetailInfo(title: item.id)));
-
-                              }
-
-                          );
-                        }).toList() ?? [],
-                      ),
-                    ),
-                    Row(
-                      children: [
-                        Expanded(
-                            child: TextField(
-                              controller: _textEditingController,
-                            )),
-                        // GestureDetector(
-                        //   child: Icon(Icons.add),
-                        //   onTap: () {
-                        //     _addToList(_textEditingController.text);
-                        //   },
-                        // )
-                      ],
-                    )
-                  ],
+                                    builder: (context) =>
+                                        DetailInfo(title: item.id)));
+                              });
+                        }).toList() ??
+                        [],
+                  ),
                 ),
-              )),
+                Row(
+                  children: [
+                    Expanded(
+                        child: TextField(
+                      controller: _textEditingController,
+                    )),
+                    // GestureDetector(
+                    //   child: Icon(Icons.add),
+                    //   onTap: () {
+                    //     _addToList(_textEditingController.text);
+                    //   },
+                    // )
+                  ],
+                )
+              ],
+            ),
+          )),
         ));
   }
 }
-
 
 // @override
 //   Widget build(BuildContext context) {
